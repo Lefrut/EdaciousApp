7f2719f5dde5993ef9c681f68ff9e88239ec0346
zoroz <zorozan21@gmail.com>
2022-08-16 11:32:37 +0300

    Initial commit

---
+++ app/src/main/java/ru/dashkevich/edaciousapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package ru.dashkevich.edaciousapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.dashkevich.edaciousapp.ui.theme.EdaciousAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EdaciousAppTheme {
+
+
+            }
+        }
+    }
+}
